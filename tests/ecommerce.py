f6732c7512ca478420118be941650fdd7332fa31
Arjun M S <arjunachu123.aa@gmail.com>
2023-03-10 22:05:38 +0530

    Initial commit

---
+++ tests/ecommerce.py
@@ -0,0 +1,57 @@
+import pytest
+from selenium.webdriver.common.by import By
+import sys
+from selenium.webdriver.common.action_chains import ActionChains
+
+@pytest.mark.usefixtures('driver')
+def test_text(driver):
+    """
+    Verify a text in the page
+    :return: None
+    """
+    driver.get('https://ecommerce-playground.lambdatest.io/')
+    driver.implicitly_wait(10)
+
+    txt = "This is a dummy website for Web Automation Testing"
+    assert txt == driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/header/div[3]/div[3]/div[1]/p/strong").text 
+
+@pytest.mark.usefixtures('driver')
+def test_register(driver):
+    """
+    Verify registration
+    :return: None
+    """
+    driver.get('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
+    
+    first_name = driver.find_element(By.NAME, "firstname")
+    first_name.send_keys("James")
+    
+    last_name = driver.find_element(By.NAME, "lastname")
+    last_name.send_keys("Ford")
+
+    email = driver.find_element(By.NAME, "email")
+    email.send_keys("jamesford@example.com")
+
+    telephone = driver.find_element(By.NAME, "telephone")
+    telephone.send_keys("1234567888")
+
+    password = driver.find_element(By.NAME, "password")
+    password.send_keys("password123")
+
+    confirm = driver.find_element(By.NAME, "confirm")
+    confirm.send_keys("password123")
+
+    agree = driver.find_element(By.NAME, "agree")
+    # Move the cursor to the agree checkbox element before clicking on it
+    actions = ActionChains(driver)
+    actions.move_to_element(agree).click().perform()
+
+
+    submit = driver.find_element(By.XPATH, "//input[@value='Continue']")
+    submit.click()
+    
+    driver.implicitly_wait(10)
+    
+    text = driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div[1]/div/div/h1").text
+    
+    assert text == "Your Account Has Been Created!"
